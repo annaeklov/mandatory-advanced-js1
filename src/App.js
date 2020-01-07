f17e2da0da0025dbe7bfa59ffdeae10eda76152a
Anna Eklöv <annaeklov@Annas-MacBook-Air.local>
2020-01-07 14:00:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client'; //måste vara med i varje fil som har kontakt med server
 
-function App() {
+class App extends React.Component {
+  render(){
+    var socket = io('http://3.120.96.16:3000');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <p>Hej</p>
     </div>
   );
+  }
+
 }
 
 export default App;
+
+/*         var socket = io('http://3.120.96.16:3000');
+
+        socket.on('connect', function () {
+            console.log("CONNECTED!")
+        });
+        socket.on('messages', function (data) {
+            console.log(data) // en lista med alla
+        });
+
+        socket.on('new_message', message => {
+            console.log("NEW MESSAGE", message) // meddelande var för sig
+        })
+        socket.emit("message", {
+            username: "AnnaEEE",
+            content: "heeeeej"
+        }); */
